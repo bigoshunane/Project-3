3514216c55aea4c6aac82a2de853054cb8bd0ee3
Shannon Kane <shannonkane90@gmail.com>
2022-04-07 20:10:11 -0400

    SK Updates

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 # import necessary libraries
+
+import pandas as pd
+import pymongo
+import json
+from pymongo import MongoClient
+
 import os
 import re
 from flask import (
@@ -17,69 +23,51 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
-uri = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+@app.route("/run-data")
+def runData():
+    with open('data/2021CountryContinent.json') as f:
+      data = json.load(f)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = client["michelin"]
+    mycol = mydb["restaurants"]
 
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    x = mycol.insert_many(data)
 
-db = SQLAlchemy(app)
-
-from .models import Pet
-
-
-# create route that renders index.html template
-@app.route("/")
-def home():
     return render_template("index.html")
 
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
+# create route that renders index.html template
 
-    return render_template("form.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+# Shannon - Query to get sorted data
+@app.route("/michelin-data")
+def getMichelinData():
 
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+    with open('data/2021CountryContinent.json') as f:
+      data = json.load(f)
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
+    cuisines = {}
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 15,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
+    for d in data:
+        if not d["Cuisine"] in cuisines:
+            cuisines[d["Cuisine"]] = 0
+        cuisines[d["Cuisine"]] += 1
 
-    return jsonify(pet_data)
+    cuisineList = []
+    for k in cuisines.keys():
+        cuisineList.append((k, cuisines[k]))
 
+    def sortFunc(e):
+        return e[1]
+
+    cuisineList.sort(key=sortFunc, reverse=True)
+
+    return jsonify(cuisineList[:10])
 
 if __name__ == "__main__":
     app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,47 @@
-function buildPlot() {
 
-    /* data route */
-  const url = "/api/pals";
-  d3.json(url).then(function(response) {
-
-    console.log(response);
-
-    const data = response;
-
-    const layout = {
-      scope: "usa",
-      title: "Pet Pals",
-      showlegend: false,
-      height: 600,
-            // width: 980,
-      geo: {
-        scope: "usa",
-        projection: {
-          type: "albers usa"
-        },
-        showland: true,
-        landcolor: "rgb(217, 217, 217)",
-        subunitwidth: 1,
-        countrywidth: 1,
-        subunitcolor: "rgb(255,255,255)",
-        countrycolor: "rgb(255,255,255)"
+d3.json("../data/2021CountryContinent.json").then((data) => {
+  console.log(data);
+});
+let ratings = ['Bib Gourmand', '1 MICHELIN Star', '2 MICHELIN Stars', '3 MICHELIN Stars']
+function buildPlots(ratings) {
+  d3.json("../data/2021CountryContinent.json").then((data) => {
+    console.log(data);
+    let countArray = []
+    // Loop through the array of awards
+    for (let i = 0; i < ratings.length; i++) {
+      // Store the award at index `i` for evaluation
+      rating = ratings[i];
+      // Initialize the count
+      let count = 0;
+      // Loop through the array of restaurant
+      for (let j = 0; j < data.length; j++) {
+        row = data[j];
+        if (row.Award == rating) {
+          count += 1
+        }
       }
+      countArray.push(count)
+    }
+    // Set up the pie chart
+    let trace2 = {
+      values: countArray,
+      labels: ratings,
+      type: 'pie',
+      name: 'Amount of Restaurants by Michelin Award Level'
     };
+    let pieData = [trace2];
+    let layout2 = {
+      title: 'Amount of Restaurants by Michelin Award Level',
+      xaxis: {
+        title: 'Awards',
 
-    Plotly.newPlot("plot", data, layout);
-  });
+      },
+    };
+    Plotly.newPlot("pie", pieData, layout2);
+
+      });
 }
+buildPlots(ratings)
 
-buildPlot();
+// Top 10 Cuisines
+// "Modern Cuisine", "Creative", "Japanese", "Traditional Cuisine", "French", "Street Food", "Italian", "Classic Cuisine", "Regional Cuisine", "Cantonese"
