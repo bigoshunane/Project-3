ca224e98f32a9de0e34ba82a957257eac528693c
bigoshunane <zeddesta@gmail.com>
2022-04-13 18:08:49 -0700

    mongoconnectionupdate

--- keys_dev.js
+++ keys_dev.js
@@ -1,3 +1,4 @@
-module.exports = {
-    mongoURI: "mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin?retryWrites=true&w=majority"
-}
+mongoose.connect("mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, );
