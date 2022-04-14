52d9971e7c1c28c7708f3dd0c7773bdaa22cc5e0
bigoshunane <zeddesta@gmail.com>
2022-04-13 22:00:51 -0700

    updatingkey

--- keys_dev.js
+++ keys_dev.js
@@ -1,4 +1,4 @@
-mongoose.connect("mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }, );
