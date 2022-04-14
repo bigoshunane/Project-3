48e96640ac738ccefb9145fe90183682467908de
bigoshunane <zeddesta@gmail.com>
2022-04-13 22:11:42 -0700

    keys

--- keys_dev.js
+++ keys_dev.js
@@ -1,4 +1,6 @@
-mongoose.connect("mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, );
+mongoose.connect(
+    process.env.MONGODB_URI = "mongodb+srv://Project-3-Team-1:password_qW1@cluster0.oqnln.mongodb.net/michelin", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+);
