feae52223b77799d67eec8757c1a06c87aa2ee11
jtbuckwalter <jtbuckwalter@gmail.com>
2022-05-21 13:35:06 -0400

    created michelin_scrape.py and notebook.

---
+++ michelin_scrape.py
@@ -0,0 +1,12 @@
+null to 783
+
+https://guide.michelin.com/us/en/restaurants
+https://guide.michelin.com/us/en/restaurants/page/6
+
+.card__menu js-restaurant__list_item .link
+
+https://guide.michelin.com/us/en/restaurants
+
+mich_description_url = '/us/en/district-of-columbia/washington-dc/restaurant/hitching-post'
+
+.js-show-description-text
